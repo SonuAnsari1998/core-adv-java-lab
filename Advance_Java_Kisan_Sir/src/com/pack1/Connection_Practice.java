d46211788cd4799941fb5f13659863fe67ffbed8
Sonu Ansari <786sonuansari123@gmail.com>
2026-01-18 21:59:47 +0530

    show table

--- Advance_Java_Kisan_Sir/src/com/pack1/Connection_Practice.java
+++ Advance_Java_Kisan_Sir/src/com/pack1/Connection_Practice.java
@@ -15,44 +15,60 @@ public class Connection_Practice {
 		try {
 			Class.forName("oracle.jdbc.driver.OracleDriver");
 			conn = DriverManager.getConnection(url, user, pwd);
-			System.out.println("Connected succefully.........");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return conn;
-	}	
+	}
 
 	public void insertData() {
 		try {
-			int id=Integer.parseInt(IO.readln("Enter Employee ID:"));
-			String fname=IO.readln("Enter Employee first name");
-			String lname=IO.readln("Enter Employee last name: ");
-			double sal=Double.parseDouble(IO.readln("Enter Salary"));
-			String add=IO.readln("Enter address");
-			Connection conn=connect();
-			Statement stm=conn.createStatement();
-			int row=stm.executeUpdate("insert into Employee values ("+id+",'"+fname+"','"+lname+"',"+sal+", '"+add+"')");
-			if(row>0) {
+			int id = Integer.parseInt(IO.readln("Enter Employee ID:"));
+			String fname = IO.readln("Enter Employee first name");
+			String lname = IO.readln("Enter Employee last name: ");
+			double sal = Double.parseDouble(IO.readln("Enter Salary"));
+			String add = IO.readln("Enter address");
+			Connection conn = connect();
+			Statement stm = conn.createStatement();
+			int row = stm.executeUpdate("insert into Employee values (" + id + ",'" + fname + "','" + lname + "'," + sal
+					+ ", '" + add + "')");
+			if (row > 0) {
 				IO.print("Data Inserted Sucessfully.......");
-			}else {
+			} else {
 				System.err.println("Data not inserted....");
 			}
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
-		catch(Exception e) {
+	}
+
+	public void showData() {
+		try {
+			Connection conn = connect();
+			Statement stm = conn.createStatement();
+			ResultSet rest = stm.executeQuery("Select * from employee");
+			while(rest.next()) {
+				IO.println(rest.getInt(1)+"\t"+rest.getString(2)+"\t"+rest.getString(3)+"\t"+rest.getDouble(4)+"\t\t"+rest.getString(5));
+			}
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 
 	public static void main(String[] args) {
-		Connection_Practice c1=new Connection_Practice();
+		Connection_Practice c1 = new Connection_Practice();
 		IO.println("1) Insert Data");
-		int choice=Integer.parseInt(IO.readln("Enter your choice"));
-		switch(choice) {
-		case 1->{
+		int choice = Integer.parseInt(IO.readln("Enter your choice"));
+		switch (choice) {
+		case 1 -> {
 			c1.insertData();
 		}
-			default ->System.err.println("Invalid choice.....");
-		};
-		
+		case 2 -> {
+			c1.showData();
+		}
+		default -> System.err.println("Invalid choice.....");
+		}
+		;
+
 	}
 }
