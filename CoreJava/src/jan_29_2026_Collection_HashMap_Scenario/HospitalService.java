fd99b6a15ab84807d63df73827468fc53c1eb48a
Sonu Ansari <786sonuansari123@gmail.com>
2026-01-29 10:54:10 +0530

    Add Hospital, HospitalService, and HospitalFinder implementation using Collection and HashMap

---
+++ CoreJava/src/jan_29_2026_Collection_HashMap_Scenario/HospitalService.java
@@ -0,0 +1,12 @@
+package jan_29_2026_Collection_HashMap_Scenario;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HospitalService {
+	ArrayList<Hospital> listOfHospital = new ArrayList<>();
+	public void addHospital(int hospitalCode, String hospitalName, List<String> listOfTreatments, String contactPerson,
+			String contactNumber, String location) {
+		
+	}
+}
