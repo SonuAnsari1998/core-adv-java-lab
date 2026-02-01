1a40840d0aa181ee994550a173aae6c8471426fb
Sonu Ansari <786sonuansari123@gmail.com>
2026-02-01 18:55:28 +0530

    Lab Test Question from Set and Map inteface

---
+++ CoreJava/src/jan_31_2026_Collection_Lab_Test_Program/Lab_Program_Set_Map_Interface.java
@@ -0,0 +1,41 @@
+package jan_31_2026_Collection_Lab_Test_Program;
+
+/*1. Problem Description
+The objective of this program is to accept multiple elements from the user, store them in a HashSet to ensure uniqueness, convert the HashSet into an array, and display all the array elements.
+
+Examples
+Example 1
+Input:
+Enter number of elements: 3
+Java Programming
+Data Structures
+Operating Systems
+Output:
+Array Elements:
+Java Programming
+Data Structures
+Operating Systems
+
+==========================================
+2. ExtractKeysfromHashMap
+Easy
+100 points
+• 125 min suggested
+Problem Description
+The objective of this program is to accept product details (Product ID and Product Name) from the user, store them in a HashMap, and then extract and display all the product IDs stored as keys in the map.
+
+Examples
+Example 1
+Input:
+Enter number of products: 3
+Enter Product ID: 101
+Enter Product Name: Pen
+Enter Product ID: 102
+Enter Product Name: Book
+Enter Product ID: 103
+Enter Product Name: Pencil
+Output:
+List of Product Keys:
+101
+102
+103*/
